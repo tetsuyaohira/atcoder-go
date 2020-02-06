77c04a4cba80a53b5b55b291f773680960f9cc71
tetsuya ohira <tetsuya.ohira@icloud.com>
2020-02-06 22:19:59 +0900

    Coins.go

--- abs/Coins.go
+++ abs/Coins.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
 func main() {
-  fmt.Println("aa")
-
+  var a, b, c, target int
+  fmt.Scan(&a, &b, &c, &target)
+  cnt := 0
+  for i:=0 ; i <= a ; i++ {
+    for j:=0 ; j<=b ; j++ {
+      for k:=0 ; k<=c ; k++ {
+        if i*500 + j*100 + k*50 == target {
+          cnt++
+        }
+      }
+    }
+  }
+  fmt.Println(cnt)
 }
 

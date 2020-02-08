062df6c13114e638f213a515afb65fab168ac064
tetsuya ohira <tetsuya.ohira@icloud.com>
2020-02-09 00:19:22 +0900

    update

--- abs/Kagami-Mochi.go
+++ abs/Kagami-Mochi.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	cnt :=0
+	fmt.Scan(&cnt)
+
+	var kagamis = make([]int, cnt)
+	for i := 0; i < cnt; i++ {
+		fmt.Scan(&kagamis[i])
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(kagamis)))
+
+	result :=0
+	var prevKagami = 101
+	for i:=0;i<cnt;i++ {
+		if kagamis[i] < prevKagami {
+			result ++
+		}
+		prevKagami = kagamis[i]
+	}
+	fmt.Println(result)
+}
